f99f086d52791f547c7f7ec5c466d7928adfa5cb
yca03 <yaochris2003@gmail.com>
2024-07-12 10:43:59 +0000

    dashbord-modif

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\ClientsRepository;
 use App\Repository\FactureRepository;
 use App\Repository\ProduitRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,10 +13,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(FactureRepository $factureRepository, ProduitRepository $produitRepository, ClientsRepository $clientsRepository): Response
+    public function index(FactureRepository $factureRepository, ProduitRepository $produitRepository, ClientsRepository $clientsRepository, UserRepository $userRepository): Response
     {
         $nombreDeFactures = $factureRepository->count();
         $nombreProduits = $produitRepository->count();
+        $nomUsers = $userRepository->count();
         $nombreClients = $clientsRepository->createQueryBuilder('c')
             ->select('COUNT(c)')
             ->getQuery()
@@ -23,6 +25,7 @@ class HomeController extends AbstractController
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'nomUsers'=>$nomUsers,
             'nombreDeFactures' => $nombreDeFactures,
             'nombreProduits' => $nombreProduits,
             'nombreClients' => $nombreClients,
