aa4783d1688e78e1f78d56d8f97425bc7b374a33
yca03 <yaochris2003@gmail.com>
2024-08-16 08:09:12 +0000

    last

---
+++ migrations/Version20240729144455.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240729144455 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE facture ADD total_ht VARCHAR(255) NOT NULL, ADD total_tva VARCHAR(255) NOT NULL, ADD total_ttc VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE facture DROP total_ht, DROP total_tva, DROP total_ttc');
+    }
+}
---
+++ migrations/Version20240729154240.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240729154240 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE facture_pro_format ADD total_ht VARCHAR(255) DEFAULT NULL, ADD total_tva VARCHAR(255) DEFAULT NULL, ADD total_ttc VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE facture_pro_format DROP total_ht, DROP total_tva, DROP total_ttc');
+    }
+}
